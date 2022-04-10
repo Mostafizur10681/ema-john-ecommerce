7208fc053de02cb2b4c2d8b741589d22b419160f
Mostafizur rahman <sourav10681@gmail.com>
2022-04-11 00:58:45 +0600

    implemented login and redirect to shop

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.css'
 import google from '../../images/google.png'
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const navigate = useNavigate();
+    const handleEmailBlur = event => {
+        setEmail(event.target.value)
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value)
+    }
+
+    if (user) {
+        navigate('/shop')
+    }
+
+    const handleSingIn = event => {
+        event.preventDefault()
+        signInWithEmailAndPassword(email, password)
+    }
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>Login</h2>
-                <form action="">
+                <form onSubmit={handleSingIn}>
                     <div className='input-group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" />
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" />
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" />
                     </div>
+                    <p style={{ color: 'red' }}> {error?.message}</p>
+                    {
+                        loading && <p>Loading..</p>
+                    }
                     <input className='form-button' type="submit" value="Login" />
                 </form>
                 <p className='newAccount'>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import google from '../../images/google.png'
-
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 const SignUp = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
     const [error, setError] = useState('')
+    const navigate = useNavigate()
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
 
     const handleEmailBlur = event => {
         setEmail(event.target.value)
@@ -17,11 +21,19 @@ const SignUp = () => {
     const handleConfirmPasswordBlur = event => {
         setConfirmPassword(event.target.value)
     }
+
+    if (user) {
+        navigate('/shop')
+    }
     const handlerCreateUser = event => {
         event.preventDefault();
         if (password !== confirmPassword) {
             setError('Your two password did not match')
         }
+        if (password.length > 6) {
+            setError('Password must be minimum 6 character')
+        }
+        createUserWithEmailAndPassword(email, password)
     }
     return (
         <div className='form-container'>
