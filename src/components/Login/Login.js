b0eafc1bdd54e8c4893c997e0cce180e47a34298
Mostafizur rahman <sourav10681@gmail.com>
2022-04-10 16:48:58 +0600

    signUp and login page added and design also doing functional

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
+import google from '../../images/google.png'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Login</h2>
+                <form action="">
+                    <div className='input-group'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="" />
+                    </div>
+                    <input className='form-button' type="submit" value="Login" />
+                </form>
+                <p className='newAccount'>
+                    New to Ema John? <Link to='/signUp'>Create New Account</Link>
+                </p>
+                <div className='orContainer'>
+                    <hr className='orLine' />
+                    <p className='or'>or</p>
+                    <hr className='orLine' />
+                </div>
+                <button className='googleSignIn' type="submit">
+                    <img src={google} alt="" />
+                    <p>Continue With Google</p>
+                </button>
+            </div>
         </div>
     );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import google from '../../images/google.png'
 
 const SignUp = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value)
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value)
+    }
+    const handleConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value)
+    }
+    const handlerCreateUser = event => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setError('Your two password did not match')
+        }
+    }
     return (
-        <div>
-            <h1>Sign Up</h1>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form onSubmit={handlerCreateUser}>
+                    <div className='input-group'>
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" />
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" />
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="Confirm-password" id="" />
+                    </div>
+                    <p style={{ color: 'red', }}>{error}</p>
+                    <input className='form-button' type="submit" value="Login" />
+                </form>
+                <p className='newAccount'>
+                    Already Have an Account? <Link to='/login'>Login</Link>
+                </p>
+                <div className='orContainer'>
+                    <hr className='orLine' />
+                    <p className='or'>or</p>
+                    <hr className='orLine' />
+                </div>
+                <button className='googleSignIn' type="submit">
+                    <img src={google} alt="" />
+                    <p>Continue With Google</p>
+                </button>
+            </div>
         </div>
     );
 };
